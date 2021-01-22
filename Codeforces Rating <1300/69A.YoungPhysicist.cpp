93104e35e42414fbc6d054abeaa29c0192fb698d
Meenakshi <57026763+meraki-mk@users.noreply.github.com>
2021-01-22 21:40:38 +0530

    69A: Young Physicist
    Codeforces Beta Round #63 (Div. 2) 
    Problem A: Young Physicist

---
+++ Codeforces Rating <1300/69A.YoungPhysicist.cpp
@@ -0,0 +1,27 @@
+// problem link: https://codeforces.com/problemset/problem/69/A
+
+// CODE
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int x=0, y=0, z=0;
+    while(n--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        x += a;
+        y += b;
+        z += c;
+    }
+    
+    if(x == y && x == z && x == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
