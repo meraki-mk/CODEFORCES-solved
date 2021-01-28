955e7fdea47d6d58585a3ebab1c3c64fb6531163
Meenakshi <57026763+meraki-mk@users.noreply.github.com>
2021-01-28 21:12:21 +0530

    Create 1459A - Red-Blue Shuffle.cpp

---
+++ Rating <= 800/1459A - Red-Blue Shuffle.cpp
@@ -0,0 +1,36 @@
+//  Codeforces Round #691 (Div. 2)
+// Problem Link : https://codeforces.com/problemset/problem/1459/A
+
+// CODE
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string r, b;
+        cin >> r >> b;
+        if(r == b)
+        {
+            cout << "EQUAL\n";
+            continue;
+        }
+        int cr=0, cb=0;
+        for(int i=0; i<n; i++)
+        {
+            if(r[i] > b[i]) cr++;
+            else if(b[i] > r[i])    cb++;
+        }
+        if(cr > cb) cout << "RED\n";
+        else if(cb > cr)    cout << "BLUE\n";
+        else    cout << "EQUAL\n";
+    }
+	return 0;
+}
+
